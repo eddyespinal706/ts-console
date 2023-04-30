4c24ebe4eea7de5d6123146109e12f2f8d0eaa44
Eddy Espinal <eddyespinal706@gmail.com>
2023-04-29 20:15:34 -0400

    first commit

---
+++ dist/index.js
@@ -0,0 +1,41 @@
+"use strict";
+const contact1 = {
+    name: 'Eddy',
+    lastName: 'Espinal',
+    phoneNumber: 8295257992,
+    email: 'eddy@me.com',
+    address: {
+        street: 'Salmos #8',
+        city: 'Santo Domingo Este',
+        country: 'Republica Dominincana',
+        zipcode: 15001
+    }
+};
+const contact2 = {
+    name: 'David',
+    lastName: 'Abreu',
+    phoneNumber: 8093885042,
+    email: 'david@me.com',
+    address: {
+        street: 'Apocalipsis #32',
+        city: 'Ditrito Nacional',
+        country: 'Republica Dominincana',
+        zipcode: 15031
+    }
+};
+const contact3 = {
+    name: 'Fernando',
+    lastName: 'Pena',
+    phoneNumber: 8093886867,
+    email: 'fernando@me.com',
+    address: {
+        street: 'Jose Cabrera #23',
+        city: 'Santo Domingo Oeste',
+        country: 'Republica Dominincana',
+        zipcode: 150321
+    }
+};
+const arr_contacts = [];
+arr_contacts.push(contact1, contact2, contact3);
+console.log(arr_contacts);
+//# sourceMappingURL=index.js.map
